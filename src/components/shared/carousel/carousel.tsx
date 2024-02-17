0f30bd9356e3d71905bece52eeaa1242d3a2de63
Yonei Sanchez <92745055+Yoneisanchezd@users.noreply.github.com>
2024-02-17 09:41:54 -0400

    Resolve conflicts and add package

--- src/components/shared/carousel/carousel.tsx
+++ src/components/shared/carousel/carousel.tsx
@@ -3,12 +3,6 @@ import { useTheme } from '@mui/material/styles';
 import Box from '@mui/material/Box';
 import MobileStepper from '@mui/material/MobileStepper';
 import Button from '@mui/material/Button';
-import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
-import SwipeableViews from 'react-swipeable-views';
-import { autoPlay } from 'react-swipeable-views-utils';
-
-const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
 
 type Props = {
     itemsByPage: number
@@ -16,17 +10,13 @@ type Props = {
 };
 
 const Swipeable = ({ itemsByPage, items }: Props) => {
-    const theme = useTheme();
-    const [activeStep, setActiveStep] = useState(1);
+    const [activeStep, setActiveStep] = useState(0);
     const [maxSteps, setMaxSteps] = useState(0)
 
     useEffect(() => {
         setMaxSteps(Math.ceil(items?.length / itemsByPage))
     }, [])
 
-    const handleStepChange = (step: number) => {
-        setActiveStep(step);
-    };
 
     const nextPage = () =>
         (activeStep + 1) * itemsByPage > items.length
@@ -37,11 +27,6 @@ const Swipeable = ({ itemsByPage, items }: Props) => {
 
     return (
         <Box >
-            <AutoPlaySwipeableViews
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
-                index={activeStep}
-                onChangeIndex={handleStepChange}
-            >
                 <Box
                     display={'flex'}
                     justifyContent={'center'}
@@ -63,8 +48,6 @@ const Swipeable = ({ itemsByPage, items }: Props) => {
                         </div>
                     ))}
                 </Box>
-
-            </AutoPlaySwipeableViews>
             <Box
                 display={'flex'}
                 justifyContent={'center'}
@@ -93,20 +76,10 @@ const Swipeable = ({ itemsByPage, items }: Props) => {
                             sx={{ display: 'none' }}
                         >
                             Next
-                            {theme.direction === 'rtl' ? (
-                                <KeyboardArrowLeft />
-                            ) : (
-                                <KeyboardArrowRight />
-                            )}
                         </Button>
                     }
                     backButton={
                         <Button size="small" sx={{ display: 'none' }}>
-                            {theme.direction === 'rtl' ? (
-                                <KeyboardArrowRight />
-                            ) : (
-                                <KeyboardArrowLeft />
-                            )}
                             Back
                         </Button>
                     }
